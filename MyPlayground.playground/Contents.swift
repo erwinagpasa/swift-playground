ae6795380e59589ffb115129593665fadfe043b8
erwinagpasa <istran.net@gmail.com>
2022-09-16 19:22:33 +0300

    Basic Function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+//func name(argumentLabel parameterName:DataType){
+//    some code...
+//}
+
+
+func addTwoNumbers(arg param1:Int, arg2 param2:Int) -> Int {
+    
+    return param1 + param2
+    
+}
+
+let sum = addTwoNumbers(arg: 2, arg2: 3)
+print(sum)
