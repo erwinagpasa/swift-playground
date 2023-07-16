b292aee67d4a23fe29e3011fb7774f2ee5707825
erwinagpasa <rootaccess000@gmail.com>
2023-07-16 12:27:18 +0300

    fixing

---
+++ MyPlayground.playground/Pages/BasicFunction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func showFirstScreen(){
+  var userDidCompleteOnboarding:Bool = false
+  var userProfileIsCreated:Bool = true
+  let status = checkUserStatus(didCompleteOnboarding: userDidCompleteOnboarding, profileIsCreate: userProfileIsCreated)
+  
+  if status == true{
+    print("Show Home Screen")
+  }else{
+    print("Show Onboarding Screen")
+  }
+}
+
+func checkUserStatus(didCompleteOnboarding:Bool, profileIsCreate:Bool) -> Bool {
+  if didCompleteOnboarding && profileIsCreate {
+    return true
+  }else{
+    return false
+  }
+}
+
+showFirstScreen()
