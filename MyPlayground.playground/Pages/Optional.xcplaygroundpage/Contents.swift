35d20ad4226b5c6d6b3bb2c325c9e41042c4a1ad
erwinagpasa <rootaccess000@gmail.com>
2023-11-02 18:22:50 +0300

    just learning

---
+++ MyPlayground.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+// Declaring a variable 'name' of type String? (optional String).
+var name: String? = "Erwin"
+
+// Assigning 'nil' to the 'name' variable, making it hold no value.
+name = nil
---
+++ MyPlayground.playground/Pages/UnwrappingOptional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var x = Int? = 3
+var y = Int? = 12
+
+// This will give as an Error
+// Error: UnwrappingOptional.xcplaygroundpage:4:16:
+// error: cannot assign value of type 'Int' to type 'Int?.Type' var y = Int? = 12
+    
+  //var result = x + y
+  //print(result)
+
+
+
+// use unwrapping optional
